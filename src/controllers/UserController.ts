90cc61a691a588854ef8cc979779787c12ad8a9c
chalormpol <chalormpol9899@gmail.com>
2025-06-07 00:58:23 +0700

    user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { Param } from "@prisma/client/runtime/library";
+import { password } from "bun";
 const prisma = new PrismaClient();
 
 export const UserController = {
@@ -10,7 +12,7 @@ export const UserController = {
     jwt: any;
   }) => {
     try {
-      const user = await prisma.user.findFirst({
+      const user = await prisma.user.findUnique({
         select: {
           id: true,
           username: true,
@@ -24,11 +26,10 @@ export const UserController = {
       });
 
       if (!user) {
-        return { message: "User not found" };
+        return { message: "Invalid username or password" };
       }
 
       const token = await jwt.sign(user);
-      console.log("Generated token:", token);
       return { token: token, user: user };
     } catch (error) {
       return { message: error };
@@ -56,12 +57,111 @@ export const UserController = {
       });
       const newData = {
         username: body.username,
-        password: body.password ?? oldUser?.password,
+        password: body.password == "" ? oldUser?.password : body.password,
       };
 
       await prisma.user.update({
         where: { id },
         data: newData,
+      });
+
+      return { message: "success" };
+    } catch (error) {
+      return { message: error };
+    }
+  },
+  list: async () => {
+    try {
+      const users = await prisma.user.findMany({
+        select: {
+          id: true,
+          username: true,
+          level: true,
+        },
+        where: {
+          status: "active",
+        },
+        orderBy: {
+          id: "desc",
+        },
+      });
+
+      return { users: users };
+    } catch (error) {
+      return { message: error };
+    }
+  },
+  create: async ({
+    body,
+  }: {
+    body: {
+      username: string;
+      password: string;
+    };
+  }) => {
+    try {
+      await prisma.user.create({
+        data: body,
+      });
+
+      return { message: "success" };
+    } catch (error) {
+      return { message: error };
+    }
+  },
+  updateUser: async ({
+    body,
+    params,
+  }: {
+    body: {
+      username: string;
+      password: string;
+      level: string;
+    };
+    params: {
+      id: string;
+    };
+  }) => {
+    try {
+      const oldUser = await prisma.user.findUnique({
+        select: { password: true },
+        where: { id: parseInt(params.id) },
+      });
+
+      const newPassword =
+        body.password.trim().length === 0 ? oldUser?.password : body.password;
+
+      const newData = {
+        username: body.username,
+        password: newPassword,
+        level: body.level,
+      };
+
+      await prisma.user.update({
+        where: { id: parseInt(params.id) },
+        data: newData,
+      });
+
+      return { message: "success" };
+    } catch (error) {
+      return { message: error };
+    }
+  },
+  remove: async ({
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }) => {
+    try {
+      await prisma.user.update({
+        where: {
+          id: parseInt(params.id),
+        },
+        data: {
+          status: "inactive",
+        },
       });
 
       return { message: "success" };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 import { cors } from "@elysiajs/cors";
 import { UserController } from "./controllers/UserController";
+import { DeviceController } from "./controllers/DeviceController";
 
 const app = new Elysia()
   .use(
@@ -15,6 +16,18 @@ const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .post("/api/user/signin", UserController.signIn)
   .put("/api/user/update", UserController.update)
+  .get("/api/user/list", UserController.list)
+  .post("/api/user/create", UserController.create)
+  .put("/api/user/updateUser/:id", UserController.updateUser)
+  .delete("/api/user/remove/:id", UserController.remove)
+
+  //
+  // Device
+  //
+  .post("/api/device/create", DeviceController.create)
+  .get("/api/device/list", DeviceController.list)
+  .put("/api/device/update/:id", DeviceController.update)
+  .delete("/api/device/remove/:id", DeviceController.remove)
 
   .listen(3001);
 
