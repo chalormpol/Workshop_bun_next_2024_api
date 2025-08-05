eaab76685cddcbfe5e669168adfa18ee6077b8e4
chalormpol <chalormpol9899@gmail.com>
2025-08-06 00:02:01 +0700

    Finishึ

--- src/index.ts
+++ src/index.ts
@@ -48,9 +48,6 @@ const checkSignIn = async ({
   }
 };
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
 const app = new Elysia()
   .use(cors())
   .use(
